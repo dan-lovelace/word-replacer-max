e9e2355a9a8544957b5bf20f373c8d060f888fec
dan-lovelace <dlovelace085@gmail.com>
2024-07-17 19:25:35 -0400

    chore(popup): hide page refresh button when popped out

--- packages/popup/src/components/RefreshRequiredToast.tsx
+++ packages/popup/src/components/RefreshRequiredToast.tsx
@@ -1,13 +1,24 @@
+import { useContext } from "preact/hooks";
+
 import TabRefreshButton from "./TabRefreshButton";
 import { useLanguage } from "../lib/language";
+import { Config } from "../store/Config";
 
 export function RefreshRequiredToast({ onClose }: { onClose: () => void }) {
+  const { isPoppedOut } = useContext(Config);
   const language = useLanguage();
 
   return (
     <span className="d-flex align-items-center gap-1">
-      {language.rules["REFRESH_REQUIRED"]}
-      <TabRefreshButton onClick={onClose}>Refresh now</TabRefreshButton>
+      {language.rules.REFRESH_REQUIRED}
+      {!isPoppedOut && (
+        /**
+         * It's not possible to be sure which tab to refresh because the
+         * extension's one is activated when this button is clicked. Let's just
+         * hide it.
+         */
+        <TabRefreshButton onClick={onClose}>Refresh now</TabRefreshButton>
+      )}
     </span>
   );
 }
