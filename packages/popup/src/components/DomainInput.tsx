af6fd04219dfeeed26e03667f3f4194854f55730
dan-lovelace <dlovelace085@gmail.com>
2024-07-18 10:50:58 -0400

    chore(popup): modify domain list input to align with larger breakpoint

--- packages/popup/src/components/DomainInput.tsx
+++ packages/popup/src/components/DomainInput.tsx
@@ -52,61 +52,73 @@ export default function DomainInput() {
   };
 
   return (
-    <div className="d-flex flex-column gap-2">
-      <div className="d-flex align-items-center gap-3">
-        <div>Effect:</div>
-        <div className="form-check m-0">
-          <input
-            checked={preferences?.domainListEffect === "allow"}
-            className="form-check-input"
-            id="allowRadio"
-            name="allow"
-            type="radio"
-            onChange={handleEffectChange("allow")}
-          />
-          <label className="form-check-label" for="allowRadio">
-            Allow
-          </label>
-        </div>
-        <div className="form-check m-0">
-          <input
-            checked={preferences?.domainListEffect === "deny"}
-            className="form-check-input"
-            id="denyRadio"
-            name="deny"
-            type="radio"
-            onChange={handleEffectChange("deny")}
-          />
-          <label className="form-check-label" for="denyRadio">
-            Deny
-          </label>
+    <div className="container-fluid gx-0 d-flex flex-column">
+      <div className="row mb-2">
+        <div className="col-12 col-lg-8">
+          <div className="d-flex align-items-center gap-3">
+            <div>Effect:</div>
+            <div className="form-check m-0">
+              <input
+                checked={preferences?.domainListEffect === "allow"}
+                className="form-check-input"
+                id="allowRadio"
+                name="allow"
+                type="radio"
+                onChange={handleEffectChange("allow")}
+              />
+              <label className="form-check-label" for="allowRadio">
+                Allow
+              </label>
+            </div>
+            <div className="form-check m-0">
+              <input
+                checked={preferences?.domainListEffect === "deny"}
+                className="form-check-input"
+                id="denyRadio"
+                name="deny"
+                type="radio"
+                onChange={handleEffectChange("deny")}
+              />
+              <label className="form-check-label" for="denyRadio">
+                Deny
+              </label>
+            </div>
+          </div>
         </div>
       </div>
-      <form className="position-relative flex-fill" onSubmit={handleFormSubmit}>
-        {Boolean(domainList?.length) && (
-          <div className="d-flex align-items-start flex-wrap gap-1 pb-2">
-            {domainList?.map((domain, idx) => (
-              <Chip
-                key={idx}
-                identifier={domain}
-                onRemove={handleRemoveClick}
-              />
-            ))}
-          </div>
-        )}
-        <input
-          className="form-control w-100"
-          enterkeyhint="enter"
-          placeholder="Add domain..."
-          type="text"
-          value={value}
-          onBlur={handleFormSubmit}
-          onInput={handleTextChange}
-        />
-        <button className="visually-hidden" type="submit">
-          Add
-        </button>
-      </form>
+      <div className="row">
+        <div className="col-12 col-lg-8">
+          <form
+            className="position-relative flex-fill"
+            onSubmit={handleFormSubmit}
+          >
+            {Boolean(domainList?.length) && (
+              <div className="d-flex align-items-start flex-wrap gap-1 pb-2">
+                {domainList?.map((domain, idx) => (
+                  <Chip
+                    key={idx}
+                    identifier={domain}
+                    onRemove={handleRemoveClick}
+                  />
+                ))}
+              </div>
+            )}
+            <input
+              className="form-control w-100"
+              enterkeyhint="enter"
+              placeholder="Add domain..."
+              type="text"
+              style={{ maxWidth: "400px" }}
+              value={value}
+              onBlur={handleFormSubmit}
+              onInput={handleTextChange}
+            />
+            <button className="visually-hidden" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
