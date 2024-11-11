5465bc79ae64841a03b16c91a94f4a5ddeff0928
dan-lovelace <dlovelace085@gmail.com>
2024-11-10 22:50:00 -0500

    feat(popup): self-hosted inter font

--- packages/content/src/index.ts
+++ packages/content/src/index.ts
@@ -1,5 +1,6 @@
-import { runStorageMigrations } from "@worm/shared/src/storage";
 import "@worm/shared/vite-env.d.ts";
+
+import { runStorageMigrations } from "@worm/shared/src/storage";
 
 import { startContentListeners } from "./lib/listeners";
 import { renderContent } from "./lib/render";
