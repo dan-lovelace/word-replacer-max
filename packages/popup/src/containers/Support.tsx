f82c52cdee058ef453dfe9daa529180c776387d3
dan-lovelace <dlovelace085@gmail.com>
2024-12-07 18:24:00 -0500

    refactor(popup): modify support page copy and add email body template

--- packages/popup/src/containers/Support.tsx
+++ packages/popup/src/containers/Support.tsx
@@ -13,9 +13,7 @@ import {
   MAILTO_CONTACT_SUPPORT_URL,
   SUBMIT_NEW_TICKET_URL,
 } from "@worm/shared/src/support";
-import {
-  CONTACT_SUPPORT_MAXIMUM_MESSAGE_LENGTH,
-} from "@worm/shared/src/validation/contact";
+import { CONTACT_SUPPORT_MAXIMUM_MESSAGE_LENGTH } from "@worm/shared/src/validation/contact";
 import {
   ApiContactSupportRequest,
   ApiContactSupportResponse,
@@ -38,7 +36,10 @@ function EmailGitHub() {
       <div className="mb-2">
         <div className="fw-medium">Email</div>
         <div>
-          <a href={MAILTO_CONTACT_SUPPORT_URL} target="_blank">
+          <a
+            href={`${MAILTO_CONTACT_SUPPORT_URL}&body=Website%20URL%3A%20%0A%0AExpected%20Behavior%3A%20%0A%0AActual%20Behavior%3A%20%0A%0AExported%20Rules%3A%20Please%20export%20the%20affected%20rules%20(as%20a%20link%20or%20file)%20and%20attach%20it%20to%20this%20message%0A%0AMore%20Details%3A%20Feel%20free%20to%20include%20any%20other%20relevant%20information`}
+            target="_blank"
+          >
             {CONTACT_SUPPORT_EMAIL}
           </a>
         </div>
@@ -214,26 +215,20 @@ export default function Support() {
             <div className={COPY_CONTAINER_COL_CLASS}>
               <div className="fs-5 fw-bold">Get Help</div>
               <p>
-                Having trouble with Word Replacer Max? We're here to help! For
-                the quickest support, please contact us via email or log an
-                issue on GitHub, as extension store tickets may be delayed
-                <sup>
-                  <a
-                    href="https://groups.google.com/a/chromium.org/g/chromium-extensions/c/V5As4co1mmI"
-                    target="_blank"
-                  >
-                    [1]
-                  </a>
-                </sup>
-                .
+                Having trouble with Word Replacer Max? We're here to help! Get
+                support by sending us an email or opening an issue on GitHub.
               </p>
               <p>
-                When reaching out, please provide:
+                When reaching out, please include:
                 <ol>
                   <li>The website URL where you're experiencing issues</li>
                   <li>
-                    Your exported rulesets (these can be exported from the
-                    Options page)
+                    Your exported rulesets (you can export these from the
+                    Options tab)
+                  </li>
+                  <li>
+                    A brief description of what you expected to happen vs. what
+                    actually happened
                   </li>
                 </ol>
               </p>
