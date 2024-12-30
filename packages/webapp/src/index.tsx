4e7cc72dbe8ed13a5a69b327b649b71852c96fee
dan-lovelace <dlovelace085@gmail.com>
2024-12-30 15:41:50 -0500

    chore(webapp): update all

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -1,16 +1,15 @@
+import "material-icons/iconfont/sharp.scss";
+import "@worm/shared/vite-env.d.ts";
+import "./style/index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import "material-icons/iconfont/sharp.scss";
-
-import "@worm/shared/vite-env.d.ts";
 
 import { App } from "./App";
 import { AuthProvider } from "./lib/auth/AuthProvider";
 import { ConnectionProvider } from "./lib/connection/ConnectionProvider";
 import { QueryProvider } from "./lib/query/QueryProvider";
 import { ToastProvider } from "./lib/toast/ToastProvider";
-import "./style/index.scss";
 
 createRoot(document.getElementById("app") as HTMLDivElement).render(
   <StrictMode>
