06ac238e2234c6338f1fb283a3bb2205d64ebe32
dan-lovelace <dlovelace085@gmail.com>
2025-02-15 18:35:02 -0500

    test(popup): csv imports with replacements

--- packages/popup/cypress/e2e/options-page/rules-import.cy.ts
+++ packages/popup/cypress/e2e/options-page/rules-import.cy.ts
@@ -7,7 +7,7 @@ describe("rules import", () => {
   });
 
   it("should add rules from a csv file", () => {
-    cy.fixture("rules-import.csv").as("rulesImport");
+    cy.fixture("rules-import-no-replacements.csv").as("rulesImport");
     s.options.rulesImport.fileInputLabel().selectFile("@rulesImport");
 
     s.layout.tabs.rules().click();
@@ -36,6 +36,59 @@ describe("rules import", () => {
           cy.contains("Word7");
           cy.contains("Word8");
         });
+      });
+    });
+  });
+
+  it("should add rules with replacements from a csv file", () => {
+    cy.fixture("rules-import-replacements.csv").as("rulesImport");
+    s.options.rulesImport.fileInputLabel().selectFile("@rulesImport");
+
+    s.layout.tabs.rules().click();
+    s.ruleRows().then((ruleRows) => {
+      expect(ruleRows.length).to.eq(5);
+
+      const thirdRow = ruleRows.eq(2);
+      const fourthRow = ruleRows.eq(3);
+      const fifthRow = ruleRows.eq(4);
+
+      cy.wrap(thirdRow).within(() => {
+        s.rules.queryInput.chips().then((chips) => {
+          expect(chips.length).to.eq(3);
+
+          cy.contains("Word1");
+          cy.contains("Word2");
+          cy.contains("Word3 and Word4");
+        });
+
+        s.rules.replacementInput
+          .textInput()
+          .should("have.value", "Replacement1");
+      });
+
+      cy.wrap(fourthRow).within(() => {
+        s.rules.queryInput.chips().then((chips) => {
+          expect(chips.length).to.eq(4);
+
+          cy.contains("Word5");
+          cy.contains("Word6");
+          cy.contains("Word7");
+          cy.contains("Word8");
+        });
+
+        s.rules.replacementInput
+          .textInput()
+          .should("have.value", "Replacement2");
+      });
+
+      cy.wrap(fifthRow).within(() => {
+        s.rules.queryInput.chips().then((chips) => {
+          expect(chips.length).to.eq(1);
+
+          cy.contains("Word3");
+        });
+
+        s.rules.replacementInput.textInput().should("have.value", "");
       });
     });
   });
