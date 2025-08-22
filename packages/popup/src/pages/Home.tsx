7a67f2596b10cb79337c6d4483f257d191ae4dd2
dan-lovelace <dlovelace085@gmail.com>
2025-08-22 13:45:10 -0600

    feat(popup): make rule list actions bottom-sticky

--- packages/popup/src/pages/Home.tsx
+++ packages/popup/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { cx } from "@worm/shared";
+
 import DomainInput from "../components/domain-input/DomainInput";
 import RuleList from "../components/rules/RuleList";
 import Account from "../containers/Account";
@@ -13,16 +15,21 @@ export default function HomePage() {
     },
   } = useConfig();
 
+  const isRulesList = preferences?.activeTab === "rules";
+
   return (
     <div
-      className="container-fluid pb-2 flex-fill overflow-y-auto"
+      className={cx(
+        "container-fluid flex-fill overflow-y-auto",
+        !isRulesList && "pb-2"
+      )}
       data-testid="home-page"
     >
       {preferences?.activeTab === "account" && <Account />}
       {preferences?.activeTab === "domains" && <DomainInput />}
       {preferences?.activeTab === "options" && <Options />}
       {preferences?.activeTab === "sharing" && <Sharing />}
-      {preferences?.activeTab === "rules" && <RuleList />}
+      {isRulesList && <RuleList />}
       {preferences?.activeTab === "support" && <Support />}
     </div>
   );
