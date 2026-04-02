b8a5e205ecc8c0ce7e6cea893917a22edb3b5a73
dan-lovelace <dlovelace085@gmail.com>
2026-04-02 06:36:51 -0600

    chore(popup): integrated option toggle

--- packages/popup/src/components/options/InputReplacement.tsx
+++ packages/popup/src/components/options/InputReplacement.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "preact/hooks";
+import { JSXInternal } from "preact/src/jsx";
 
 import {
   getCommandShortcut,
@@ -6,14 +7,27 @@ import {
   parseShortcutKeys,
   ShortcutKey,
 } from "@worm/shared/src/browser";
+import { storageSetByKeys } from "@worm/shared/src/storage";
 
 import { Indented } from "../../containers/Indented";
+import { useLanguage } from "../../lib/language";
+import { useConfig } from "../../store/Config";
 
+import { useToast } from "../alert/useToast";
 import TextButton from "../button/TextButton";
 
 export default function InputReplacement() {
   const [shortcutKeys, setShortcutKeys] = useState<ShortcutKey[]>([]);
 
+  const {
+    storage: {
+      sync: { preferences },
+    },
+  } = useConfig();
+  const language = useLanguage();
+  const { showToast } = useToast();
+
+  const isActive = !!preferences?.inputReplacement.active;
   const shortcutExists = shortcutKeys.length > 0;
 
   useEffect(() => {
@@ -22,10 +36,44 @@ export default function InputReplacement() {
     });
   }, []);
 
+  const handleActiveChange = (
+    event: JSXInternal.TargetedEvent<HTMLInputElement, Event>
+  ) => {
+    const newPreferences = Object.assign({}, preferences);
+    const newInputReplacement = Object.assign(
+      {},
+      newPreferences.inputReplacement
+    );
+
+    newInputReplacement.active = event.currentTarget.checked;
+    newPreferences.inputReplacement = newInputReplacement;
+
+    storageSetByKeys({ preferences: newPreferences });
+  };
+
   return (
     <div className="input-replacement">
+      <div
+        className="form-check form-switch ps-0 d-flex align-items-center gap-2"
+        data-testid="input-replacement-input-wrapper"
+      >
+        <input
+          checked={isActive}
+          className="form-check-input m-0"
+          data-testid="input-replacement-toggle-button"
+          id="input-replacement-enabled-checkbox"
+          role="switch"
+          type="checkbox"
+          onChange={handleActiveChange}
+        />
+        <label
+          className="form-check-label user-select-none fw-medium"
+          for="input-replacement-enabled-checkbox"
+        >
+          Replace text where you type
+        </label>
+      </div>
       <Indented data-testid="input-replacement-description">
-        <div className="fw-medium">Replace text where you type</div>
         <div className="fs-sm">
           Replacements don't run automatically in text fields or rich-text
           editors. Use a keyboard shortcut to apply them on demand.
